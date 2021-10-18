9df1aeaf8f92b0374ae8cb50f553b07d65001313
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-18 15:12:06 +0530

    Initial commit

---
+++ main.sh
@@ -0,0 +1,4 @@
+brew list and brew list --cask
+Running brew list will show a list of all your installed Homebrew packages.
+
+In addition, brew list --cask will provide the items installed using Homebrew Cask.
